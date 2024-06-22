ebd39e91c3053c84b9d8d005dcbba83f846ffd8d
mio <mio.run.trip@gmail.com>
2024-06-22 14:09:49 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+
+const today = new Date();
+
+const [year, month, day] = [
+    today.getFullYear(),
+    today.getMonth()+1,
+    today.getDate(),  
+];
+
+console.log( year + '年' + month + '月' + day + '日');
